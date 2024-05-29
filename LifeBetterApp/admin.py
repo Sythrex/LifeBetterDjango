3b1ae14eed1808d3f9db8ece4a14e71e6308df4d
Ela04 <ga.antunez@duocuc.cl>
2024-05-29 17:58:57 -0400

    Cambio en models.py tablas db

--- LifeBetterApp/admin.py
+++ LifeBetterApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Encomienda, EspacioComun, Visitante
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Encomienda)
+admin.site.register(EspacioComun)
+admin.site.register(Visitante)
