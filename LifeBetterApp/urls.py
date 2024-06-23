2e1e671fe49ae98fd0d03395b9b68f3a56c8383f
Ela04 <ga.antunez@duocuc.cl>
2024-06-23 00:39:00 -0400

    CrearEcomun en Forms para admin

--- LifeBetterApp/urls.py
+++ LifeBetterApp/urls.py
@@ -1,34 +1,49 @@
-from django.urls import path, include
+from django.urls import path
 from django.urls import reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # ------------------------------------------------
+    #                     SITIO
+    # ------------------------------------------------
     path('', views.home, name='home'),
     path('sitio/login/', views.login_view, name='login'),
     path('sitio/servicio/', views.servicio, name='servicio'),
     path('nosotros/', views.nosotros, name='nosotros'),
-    path('conserje/', views.conserje, name='conserje'),
-    path('unauthorized/', views.unauthorized, name='unauthorized'),
 
-    # Rutas para el restablecimiento de contraseña
+    # ------------------------------------------------
+    #             RESTABLECIMIENTO DE CONTRASEÑA
+    # ------------------------------------------------
     path('password_reset/', views.password_reset, name='password_reset'),
     path('password_reset/done/', views.done, name='done'),
     path('password_reset/<uidb64>/<token>/', views.confirm, name='confirm'),
     path('password_reset/done/', views.complete, name='complete'),
+    
+    # ------------------------------------------------
+    #                     WEBPAY
+    # ------------------------------------------------
+    path('webpay/plus/commit/', views.webpay_plus_commit, name='webpay-plus-commit'),
+    path('webpay/plus/create/', views.webpay_plus_create, name='webpay-plus-create'),
 
-    # Gestion vista residente
+    # ------------------------------------------------
+    #                  RESIDENTE
+    # ------------------------------------------------
     path('residente/', views.residente, name='residente'),
     path('avisos/', views.avisos, name='avisos'),
     path('encoresidente/', views.encoresidente, name='encoresidente'),
     path('perfil/', views.perfil, name='perfil'),
-    path('residente/perfil/editar_perfil/', views.editar_perfil, name='editar_perfil'),
+    path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
     path('reclamos/', views.reclamos, name='reclamos'),
-    path('residente/crear/crear/', views.crear, name='crear'),
-    path('residente/visita/', views.visita, name='visita_residente'),
+    path('visitas/', views.visita, name='visita_residente'),
+    path('crear/', views.crear, name='crear'), 
+    path('gcomunes/', views.gcomunes, name='gcomunes'),
+    path('multasrev/', views.multasrev, name='multasrev'),
 
-    #CALENDAR
-    path('residente/ecomun/espaciocomun/', views.espaciocomun, name='espaciocomun'),
+    # ------------------------------------------------
+    #                  ESPACIO COMUN
+    # ------------------------------------------------
+    path('residente/ecomun/', views.espaciocomun, name='espaciocomun'),
     path('residente/ecomun/crear_reservacion/', views.crear_reservacion, name='crear_reservacion'),
     path('residente/ecomun/listar_reservaciones/', views.listar_reservaciones, name='listar_reservaciones'),
     path('residente/ecomun/detalle_espacio/<int:id_ec>/', views.detalle_espacio, name='detalle_espacio'),
@@ -36,27 +51,21 @@ urlpatterns = [
     path('residente/ecomun/eliminar_reservacion/<int:id_reservacion>/', views.eliminar_reservacion, name='eliminar_reservacion'),
     path('residente/ecomun/validar_disponibilidad/', views.validar_disponibilidad, name='validar_disponibilidad'),
 
-    path('gcomunes/', views.gcomunes, name='gcomunes'),
-    path('resumen/', views.resumen, name='resumen'),
-    path('multasrev/', views.multasrev, name='multasrev'),
-    # webpay
-    path('webpay/plus/commit/', views.webpay_plus_commit, name='webpay-plus-commit'),
-    path('webpay/plus/create/', views.webpay_plus_create, name='webpay-plus-create'),
-
-    # administrador
+    # ------------------------------------------------
+    #                 ADMINISTRADOR
+    # ------------------------------------------------
     path('adminedificio/', views.admin, name='adminedificio'),
-    path('administrador/residente/crear/', views.crear_residente, name='crear_residente'),
-    path('administrador/empleado/crear/', views.crear_empleado, name='crear_empleado'),
-    path('administrador/creardepartamento/', views.creardepartamento, name='creardepartamento'),
-    path('administrador/crear_ecomun/', views.crear_ecomun, name='crear_ecomun'),
-    path('administrador/multasadmin/', views.multasadmin, name='multasadmin'),
-    path('crear_multa/', views.crear_multa, name='crear_multa'),
-    path('sitio/gastoscomunes/', views.gastoscomunes, name='gastoscomunes'),
-    
-    # GESTIÓN DE USUARIOS
-    path('logout/', views.salir, name='logout'),  # Ruta para la vista de cerrar sesión
+    path('adminedificio/residente/crear/', views.crear_residente, name='crear_residente'),
+    path('adminedificio/empleado/crear/', views.crear_empleado, name='crear_empleado'),
+    path('adminedificio/creardepartamento/', views.creardepartamento, name='creardepartamento'),
+    path('adminedificio/crear_ecomun/', views.crear_ecomun, name='crear_ecomun'),
+    path('adminedificio/multasadmin/', views.multasadmin, name='multasadmin'),
+    path('adminedificio/crear_multa/', views.crear_multa, name='crear_multa'),
 
-    # CONSERJE
+    # ------------------------------------------------
+    #                   CONSERJE
+    # ------------------------------------------------
+    path('conserje/', views.conserje, name='conserje'),
     path('conserje/encomienda/', views.encomienda, name='encomienda'),
     path('conserje/visita/', views.visita, name='visita_conserje'),
     path('conserje/visita/crear', views.nueva_visita, name='nueva_visita'),
@@ -69,5 +78,8 @@ urlpatterns = [
     path('conserje/bitacora/crear', views.crear_bitacora, name='crear_bitacora'),
     path('conserje/reclamos/', views.reclamos, name='reclamos'),
 
-
+    # ------------------------------------------------
+    #              GESTIÓN DE USUARIOS
+    # ------------------------------------------------
+    path('logout/', views.salir, name='logout'), 
 ]
